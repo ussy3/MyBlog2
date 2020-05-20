6e494f0bb82e4c85c356fdbff17ed81554dc288b
EC2 Default User <ec2-user@ip-172-31-46-221.ap-northeast-1.compute.internal>
2020-05-20 07:02:59 +0000

    Add root

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'members#index'
   resources :members
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
